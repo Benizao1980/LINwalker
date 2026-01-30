9e79974e076d7a4b643a723db65c9271650a43b0
Ben Pascoe <benizao@gmail.com>
2026-01-30 22:32:01 +0000

    Add SOURCE_COLOURS dictionary to palette.py

---
+++ palette.py
@@ -0,0 +1,10 @@
+# linwalker/palette.py
+
+SOURCE_COLOURS = {
+    "chicken": "#E6C229",       # mustard yellow
+    "ruminant": "#4F7F6A",      # muted green
+    "pig": "#D88C9A",           # dusty pink
+    "wild bird": "#7A5195",     # aubergine purple
+    "other animal": "#8E8E8E",  # grey
+    "human": "#1F1F1F"          # near-black
+}
